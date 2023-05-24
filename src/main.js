f326d4a2fd265a16a2a2133d01a89834ec635272
Xprim3 <129306389+Xprim3@users.noreply.github.com>
2023-05-24 12:35:57 +0200

    secound commit

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import '../styles/modern-normalization.css';
 import '../styles/general.css';
 import '../styles/section1.css';
 import '../styles/section2.css';
+import '../styles/section3.css';
 
 
